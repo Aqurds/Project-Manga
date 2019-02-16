ec956022580963606c265cc3edd34db81fefb478
Aqurds <omarf1320@gmail.com>
2019-02-16 23:35:35 +0600

    Uploading project manga

---
+++ static/themes/home/js/back-to-top3860.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$('body').append('<div id="top">Back to Top</div>');$(window).scroll(function(){if($(window).scrollTop()!=0){$('#top').fadeIn();}else{$('#top').fadeOut();}});$('#top').click(function(){$('body,html').animate({scrollTop:0},300);});});

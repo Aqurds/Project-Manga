1d2cdabc3256e16e79297a7e9ac80a4c951a0cbd
Aqurds <omarf1320@gmail.com>
2019-02-20 22:39:54 +0600

    Flask integrate

--- app.py
+++ app.py
@@ -1,19 +1,153 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for, request, session, redirect, abort
+from flask_pymongo import PyMongo, pymongo
+import bcrypt
 
 app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'nineanimes'
+app.config['MONGO_URI'] = 'mongodb://aqurds:2252010baby@ds115436.mlab.com:15436/nineanimes'
+
+mongo = PyMongo(app)
+
 
 
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    items = mongo.db.items
+    all_items = items.find().sort('views', pymongo.DESCENDING).limit(56)
+    return render_template("index.html", all_items = all_items)
+
 
 @app.route('/home_json_tooltips/')
 def home_json_tooltips():
     return render_template('home_json_tooltips.html')
 
 
+@app.route('/manga/')
+def manga():
+    return render_template('manga.html')
+
+
+@app.route('/manga-id/')
+def manga_id():
+    return render_template('manga-id.html')
+
+
+@app.route('/manga-id-chapter/')
+def manga_id_chapter():
+    return render_template('manga-id-chapter.html')
+
+
+@app.route('/register/', methods=['POST', 'GET'])
+def register():
+    if session:
+        return redirect(url_for('account'))
+    if request.method == 'POST':
+        users = mongo.db.users
+        current_user = users.find_one({'name' : request.form['username']})
+
+        if current_user is None:
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'password' : hashpass, 'displayname' : request.form['displayname'], 'email' : request.form['email']})
+            session['username'] = request.form['username']
+            return redirect(url_for('account'))
+        error_message = 'Username already exist, please choose different one!'
+        return render_template('register.html', error_message = error_message)
+    return render_template('register.html')
+
+
+
+
+@app.route('/login/', methods=['POST', 'GET'])
+def login():
+    if session:
+        return redirect(url_for('account'))
+    if request.method == 'POST':
+        users = mongo.db.users
+        login_user = users.find_one({'name' : request.form['username']})
+
+        if login_user:
+            if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password']) == login_user['password']:
+            # if  bcrypt.check_password_hash(login_user['password'], request.form['password']):
+                session['username'] = request.form['username']
+                return redirect(url_for('account'))
+            error_message = 'Invalid username or password, Please try again!'
+            return render_template('login.html', error_message = error_message)
+
+    return render_template('login.html')
+
+
+
+
+
+@app.route('/data/')
+def data():
+    # item = mongo.db.items
+    # getty = item.find_one({"data_id": "9m40"})
+    # return getty['title']
+
+    items = mongo.db.items
+    all_items = items.find()
+
+    return render_template('data.html', all_items=all_items)
+
+
+@app.route('/account/')
+def account():
+    if session:
+        return render_template('account.html')
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/logout/')
+def logout():
+    if session:
+        session.clear()
+        return redirect(url_for('logout'))
+    else:
+        return redirect(url_for('home'))
+
+
+@app.route('/update/')
+def update():
+    if session:
+        return render_template('update.html')
+    else:
+        return redirect(url_for('login'))
+
+
+
+@app.route('/password-change/')
+def password_change():
+    if session:
+        return render_template('password-change.html')
+    else:
+        return redirect(url_for('login'))
+
+
+
+@app.route('/bookmark/')
+def bookmark():
+    if session:
+        return render_template('bookmark.html')
+    else:
+        return redirect(url_for('login'))
+
+
+
+@app.route('/history/')
+def history():
+    if session:
+        return render_template('history.html')
+    else:
+        return redirect(url_for('login'))
+
+
+
+
 
 if __name__ == '__main__':
+    app.secret_key = '0f9dc56d2288afa6e10b8d97577fe25b'
     app.run(debug=True)
--- static/themes/home/js/custom_story.js
+++ static/themes/home/js/custom_story.js
@@ -1,4 +1,109 @@
-function movetolistchapter(){var divPosition=$('#chapter').offset();$('html, body').animate({scrollTop:divPosition.top},0);};function expandcontent(){document.getElementById('noidungm').style.maxHeight='none';document.getElementById("content_more").style.display="none";document.getElementById("content_less").style.display="initial";}
-function lesscontent(){document.getElementById('noidungm').style.maxHeight='300px';document.getElementById("content_less").style.display="none";document.getElementById("content_more").style.display="initial";}
-$(document).ready(function(e){$('.content_more').click(function(e){expandcontent();var date=new Date();date.setTime(date.getTime()+(30*24*60*60*1000));$.cookie("content_more","more",{expires:date,path:"/"});});$('.content_less').click(function(e){lesscontent();var date=new Date();date.setTime(date.getTime()+(30*24*60*60*1000));$.cookie("content_more","less",{expires:date,path:"/"});});$(window).bind("load",function(){if($.cookie("content_more")=="less"||tooltypejs=='mobile'){lesscontent();}});});$(document).ready(function(){if(!$.cookie("vote_"+$postid)){$('.rate_row').starwarsjs({stars:5,count:1,default_stars:$votepointstar,on_select:function(datares){$('.rate_row_result').starwarsjs({stars:5,count:1,default_stars:datares,disable:0});document.getElementById("rate_row").style.display="none";document.getElementById("rate_row_cmd").innerHTML="Thanks for your vote!";var date=new Date();date.setTime(date.getTime()+(3*60*60*1000));$.cookie("vote_"+$postid,"voted",{expires:date,path:"/"});jQuery.ajax({type:'POST',data:{'rate':datares,'idmanga':$postid},url:$ddrate});}});}else{$('.rate_row').starwarsjs({stars:5,count:1,default_stars:$votepointstar,disable:0});}});function addnote(){if($lg==false){alert('Please login to use this function !');return;}
-jQuery.ajax({url:$ddbookmark,type:"POST",data:{'storyid':$postid},success:function(data){$("#Bookmark").html("Followed");},error:function(xhr){}});};
+// Chapte-list jumping code
+function movetolistchapter() {
+    var divPosition = $('#chapter').offset();
+    $('html, body').animate({
+        scrollTop: divPosition.top
+    }, 300);
+};
+// Chapter-list jumping code end
+
+
+// Show-more & Show-less content button code
+function expandcontent() {
+    document.getElementById('noidungm').style.maxHeight = 'none';
+    document.getElementById("content_more").style.display = "none";
+    document.getElementById("content_less").style.display = "initial";
+}
+
+function lesscontent() {
+    document.getElementById('noidungm').style.maxHeight = '300px';
+    document.getElementById("content_less").style.display = "none";
+    document.getElementById("content_more").style.display = "initial";
+}
+$(document).ready(function(e) {
+    $('.content_more').click(function(e) {
+        expandcontent();
+        var date = new Date();
+        date.setTime(date.getTime() + (30 * 24 * 60 * 60 * 1000));
+        $.cookie("content_more", "more", {
+            expires: date,
+            path: "/"
+        });
+    });
+    $('.content_less').click(function(e) {
+        lesscontent();
+        var date = new Date();
+        date.setTime(date.getTime() + (30 * 24 * 60 * 60 * 1000));
+        $.cookie("content_more", "less", {
+            expires: date,
+            path: "/"
+        });
+    });
+    $(window).bind("load", function() {
+        if ($.cookie("content_more") == "less" || tooltypejs == 'mobile') {
+            lesscontent();
+        }
+    });
+});
+// Show-more & Show-less content button code end
+
+
+
+$(document).ready(function() {
+    if (!$.cookie("vote_" + $postid)) {
+        $('.rate_row').starwarsjs({
+            stars: 5,
+            count: 1,
+            default_stars: $votepointstar,
+            on_select: function(datares) {
+                $('.rate_row_result').starwarsjs({
+                    stars: 5,
+                    count: 1,
+                    default_stars: datares,
+                    disable: 0
+                });
+                document.getElementById("rate_row").style.display = "none";
+                document.getElementById("rate_row_cmd").innerHTML = "Thanks for your vote!";
+                var date = new Date();
+                date.setTime(date.getTime() + (3 * 60 * 60 * 1000));
+                $.cookie("vote_" + $postid, "voted", {
+                    expires: date,
+                    path: "/"
+                });
+                jQuery.ajax({
+                    type: 'POST',
+                    data: {
+                        'rate': datares,
+                        'idmanga': $postid
+                    },
+                    url: $ddrate
+                });
+            }
+        });
+    } else {
+        $('.rate_row').starwarsjs({
+            stars: 5,
+            count: 1,
+            default_stars: $votepointstar,
+            disable: 0
+        });
+    }
+});
+
+function addnote() {
+    if ($lg == false) {
+        alert('Please login to use this function !');
+        return;
+    }
+    jQuery.ajax({
+        url: $ddbookmark,
+        type: "POST",
+        data: {
+            'storyid': $postid
+        },
+        success: function(data) {
+            $("#Bookmark").html("Followed");
+        },
+        error: function(xhr) {}
+    });
+};
